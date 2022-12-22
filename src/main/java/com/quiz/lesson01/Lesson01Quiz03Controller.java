e535a25de67ecf0ea236f057427c5196c14a1369
betterthani <107106107@naver.com>
2022-12-23 01:03:26 +0900

    DB연동 퀴즈1

--- src/main/java/com/quiz/lesson01/Lesson01Quiz03Controller.java
+++ src/main/java/com/quiz/lesson01/Lesson01Quiz03Controller.java
@@ -9,6 +9,7 @@ public class Lesson01Quiz03Controller {
 	// http://localhost:8080/lesson01/quiz03/1
 	@RequestMapping("/lesson01/quiz03/1")
 	public String quiz03() {
+		// ViewResolver - @ResponseBody 없을 때 뷰의 경로를 찾는다.
 		return "lesson01/quiz03";
 	}
 }
