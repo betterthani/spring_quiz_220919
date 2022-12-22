8b3dc881e20ad727ee36f688802ac91abf47dfe4
betterthani <107106107@naver.com>
2022-12-22 16:34:36 +0900

    초기 환경 셋팅

---
+++ src/main/java/com/quiz/lesson01/Info.java
@@ -0,0 +1,30 @@
+package com.quiz.lesson01;
+
+import java.util.Map;
+
+public class Info {
+	
+	private String title; // key
+	private String user;
+	private String content;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) { //value
+		this.title = title;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
